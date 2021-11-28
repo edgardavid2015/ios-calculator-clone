6ca42e40d6887527bca42d083b467c7a81a4267d
Edgar Guaman <edguitar2164@gmail.com>
2021-11-28 03:50:16 -0500

    Adding functionality and project is finished

--- iOSCalculatorClone/src/screens/CalculatorScreen.tsx
+++ iOSCalculatorClone/src/screens/CalculatorScreen.tsx
@@ -1,54 +1,70 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Text, View } from 'react-native'
 import { CalcButton } from '../components/CalcButton'
 import { styles } from '../theme/appTheme'
+import { useCalculator } from '../hooks/useCalculator';
 
 export const CalculatorScreen = () => {
 
-    const [previousNumber, setPreviousNumber] = useState('0')
-    const [currentNumber, setCurrentNumber] = useState('0')
-
-    const clear = () => {
-        setCurrentNumber('0')
-    }
+    const {
+        previousNumber,
+        currentNumber,
+        clearEntry,
+        createNumber,
+        changeToNegativeOrPositive,
+        deleteLastEntry,
+        divideNumbers,
+        multiplyNumbers,
+        substractNumbers,
+        addNumbers,
+        showResult
+    } = useCalculator();
 
     return (
         <View style={ styles.resultContainer }>
-            <Text style={ styles.smallResult }>{ previousNumber }</Text>
-            <Text style={ styles.result }>{ currentNumber }</Text>
+            {
+                (previousNumber !== '0') && (<Text style={ styles.smallResult }>{ previousNumber }</Text>)
+            }       
+            <Text
+                style={ styles.result }
+                numberOfLines= {1}
+                adjustsFontSizeToFit
+            >
+                { currentNumber }
+            </Text>
 
             <View style={ styles.rowButtons }>
-                <CalcButton text="C" color="#9B9B9B" action={ clear}/>
-                <CalcButton text="+/-" color="#9B9B9B"/>
-                <CalcButton text="del" color="#9B9B9B"/>
-                <CalcButton text="÷" color="#FF9427"/>
+                <CalcButton text="C" color="#9B9B9B" action={ clearEntry }/>
+                <CalcButton text="+/-" color="#9B9B9B" action={ changeToNegativeOrPositive }/>
+                <CalcButton text="del" color="#9B9B9B" action={ deleteLastEntry }/>
+                <CalcButton text="÷" color="#FF9427" action={ divideNumbers }/>
             </View>
 
             <View style={ styles.rowButtons }>
-                <CalcButton text="7"/>
-                <CalcButton text="8"/>
-                <CalcButton text="9"/>
-                <CalcButton text="X" color="#FF9427"/>
+                <CalcButton text="7" action={ createNumber }/>
+                <CalcButton text="8" action={ createNumber }/>
+                <CalcButton text="9" action={ createNumber }/>
+                <CalcButton text="X" color="#FF9427" action={ multiplyNumbers }/>
             </View>
 
             <View style={ styles.rowButtons }>
-                <CalcButton text="4"/>
-                <CalcButton text="5"/>
-                <CalcButton text="6"/>
-                <CalcButton text="–" color="#FF9427"/>
+                <CalcButton text="4" action={ createNumber }/>
+                <CalcButton text="5" action={ createNumber }/>
+                <CalcButton text="6" action={ createNumber }/>
+                <CalcButton text="-" color="#FF9427" action={ substractNumbers }/>
             </View>
 
             <View style={ styles.rowButtons }>
-                <CalcButton text="1"/>
-                <CalcButton text="2"/>
-                <CalcButton text="3"/>
-                <CalcButton text="+" color="#FF9427"/>
+                <CalcButton text="1" action={ createNumber }/>
+                <CalcButton text="2" action={ createNumber }/>
+                <CalcButton text="3" action={ createNumber }/>
+                <CalcButton text="+" color="#FF9427" action={ addNumbers }/>
             </View>
 
             <View style={ styles.rowButtons }>
-                <CalcButton text="0" width/>
-                <CalcButton text="."/>
-                <CalcButton text="=" color="#FF9427"/>
+                <CalcButton text="0" width action={ createNumber }/>
+                <CalcButton text="." action={ createNumber }/>
+                <CalcButton text="=" color="#FF9427" action={ showResult }/>
             </View>
 
         </View>
